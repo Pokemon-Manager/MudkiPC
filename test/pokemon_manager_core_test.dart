07a76097decc8456555a1e75f50e2b22c5f100dd
DrRetro <colindrwho@gmail.com>
2024-06-22 11:54:46 -0400

    Workflow and Test Update

--- test/pokemon_manager_core_test.dart
+++
@@ -1,6 +0,0 @@
-import 'package:pokemon_manager/pokemon_manager.dart';
-
-void main() {
-  PC pc = PC(pokemons: [], species: {});
-  pc.openFolder("C:/Users/Colly/OneDrive/Documents/Pokemon/pkmdb");
-}
